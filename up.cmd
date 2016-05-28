f4078455594d113fb8c3f7816311091bdc5827d7
cca <cca@aktos>
2016-05-29 01:51:57 +0300

    simplified up.cmd script

--- up.cmd
+++ up.cmd
@@ -1,13 +1,7 @@
 @echo off
 
 set /p IP=<%~dp0\ip-settings.txt
-set DNS=192.168.9.105
-set ADAPTER_NAME=aea-tap
+set ADAPTER_NAME=%1
 
-echo Setting DNS to %DNS% on %ADAPTER_NAME%
-netsh interface ip set dns name="%ADAPTER_NAME%" static %DNS% primary
-netsh interface ip add dns name="%ADAPTER_NAME%" 8.8.8.8 index=2
-ipconfig /flushdns
-
-echo Setting ip address to %IP%
+echo Setting ip address to %IP% on adapter %ADAPTER_NAME%
 netsh interface ip set address "%ADAPTER_NAME%" static %IP% 255.255.255.0
